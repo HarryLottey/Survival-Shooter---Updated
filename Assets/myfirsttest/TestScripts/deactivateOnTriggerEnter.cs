1baadff3cc6a7d95725d1a22ea5f794d81c60c04
HarryLottey <harry.lottey@gmail.com>
2017-11-20 12:09:01 +1100

    Added TestTools

---
+++ Assets/myfirsttest/TestScripts/deactivateOnTriggerEnter.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class deactivateOnTriggerEnter : MonoBehaviour
+{
+    void OnTriggerEnter()
+    {
+        gameObject.SetActive(false);
+    }
+}
